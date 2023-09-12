b7a23550b32429623567c7da5a36e6c32bd059bc
Riccardo Riggi <ghirowinformatica@gmail.com>
2023-09-12 19:28:39 +0200

    Update table.sql

--- table.sql
+++ table.sql
@@ -2,15 +2,18 @@ CREATE TABLE ccp_cache_request (
     id int(10) NOT NULL,
     idTile varchar(255) NOT NULL,
     eventDate datetime NOT NULL DEFAULT current_timestamp()
-) CREATE TABLE ccp_cache_request_sto (
+);
+ CREATE TABLE ccp_cache_request_sto (
     id int(10) NOT NULL DEFAULT 0,
     idTile varchar(255) NOT NULL,
     eventDate datetime NOT NULL DEFAULT current_timestamp()
-) CREATE TABLE ccp_osm_request (
+);
+ CREATE TABLE ccp_osm_request (
     id int(10) NOT NULL,
     idTile varchar(255) NOT NULL,
     eventDate datetime NOT NULL DEFAULT current_timestamp()
-)
+);
+
 ALTER TABLE
     ccp_cache_request
 ADD
